6f31e16dc19ea1ccfa0d8e06fd68fe9f87527943
Graham Lillico <63392565+glillico@users.noreply.github.com>
2023-04-10 23:39:51 +0100

    Install rosetta, run playbook and gickup.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo '# Starting'
+
 echo '## Setup python virtual environment'
 mkdir -p ~/Development/Python_VENVs ~/Development/GitHub
 cd ~/Development/Python_VENVs
@@ -44,15 +46,23 @@ cd ~/Development/macup/mac-dev-playbook && echo "ansible_collections" >> .gitign
 echo '### Install ansible requirements'
 ansible-galaxy install -r requirements.yml
 
+echo '## Install Rosetta 2'
+sudo softwareupdate --install-rosetta
+
+echo '## Run ansible playbook'
+ANSIBLE_CONFIG=~/Development/macup/mac-dev-playbook/ansible.cfg ansible-playbook -i ~/Development/macup/mac-dev-playbook/inventory ~/Development/macup/mac-dev-playbook/main.yml -K
+
 echo '## Setup gickup'
-echo '## Setup gickup environment'
+echo '### Setup gickup environment'
 mkdir -p ~/Development/gickup
 cd ~/Development/gickup
-echo '## Download gickup binary'
-wget https://github.com/cooperspencer/gickup/releases/download/v0.10.14/gickup_0.10.14_darwin_amd64.tar.gz
-tar zxvf gickup_0.10.14_darwin_amd64.tar.gz
-echo 'Configure gickup'
+echo '### Download gickup binary'
+wget https://github.com/cooperspencer/gickup/releases/download/v0.10.14/gickup_0.10.14_darwin_arm64.tar.gz
+tar zxvf gickup_0.10.14_darwin_arm64.tar.gz
+echo '### Configure gickup'
 curl https://gist.githubusercontent.com/glillico/f6b4866741ce7640ab56b3d78dd05e2d/raw/cfe1c43d95bbbe8720692d71146f1631b4e8a69d/conf.yml -o conf.yml
+echo '## Run gickup'
+~/Development/gickup/gickup ~/Development/gickup/conf.yml
 
 echo '## Setup ssh keys'
 echo '### Clone ssh repository'
@@ -73,13 +83,10 @@ git clone https://github.com/glillico/homelab.git ~/Development/GitHub/homelab
 echo '### Link to homelab repository'
 ln -s ~/Development/GitHub/homelab
 
-echo '## Done'
+echo '## Complete setup of backblaze'
+open '/opt/homebrew/Caskroom/backblaze/8.5.0.664/Backblaze Installer.app'
 
-echo
-echo Run the below when ready.
-echo 'source ~/Development/Python_VENVs/macup/bin/activate'
-echo 'ANSIBLE_CONFIG=~/Development/macup/mac-dev-playbook/ansible.cfg ansible-playbook -i ~/Development/macup/mac-dev-playbook/inventory ~/Development/macup/mac-dev-playbook/main.yml -K'
-echo
-echo Edit ~/Development/gickup/conf.yml and add in your github token.
-echo Then run the below to sync all github repositories.
-echo '~/Development/gickup/gickup ~/Development/gickup/conf.yml'
+echo '## Purge cache'
+python3 -m pip cache purge
+
+echo '# Done'
